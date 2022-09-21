18e146f38c4be116b37162442b326c4754401d10
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-09-21 20:05:40 +0900

    [Feature/#13] useForm custom hook 구현 (#14)
    * feat #13: 절대경로 리스트 추가
    
    * style #13: disabled 속성 추가
    
    * feat #13: 비동기 테스트를 위한 util 함수 구현
    
    * feat #13: ErrorText 컴포넌트 구현
    
    * feat #13: useForm custom hook 구현
    
    * feat #13: storybook 절대경로 리스트 추가
    
    * refactor #13: 중복 코드 줄이기 위해 타입 추론이 가능하도록 로직 위치 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,6 +19,8 @@ module.exports = {
 				...config.resolve,
 				alias: {
 					'@components': path.resolve(__dirname, '../src/components'),
+					'@hooks': path.resolve(__dirname, '../src/hooks'),
+					'@utils': path.resolve(__dirname, '../src/utils'),
 				},
 			},
 		};
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
+import useForm from '@hooks/useForm';
+import sleep from '@utils/sleep';
 import Input from './Input';
 import Button from './Button';
+import ErrorText from './ErrorText';
 
 const Container = styled.form`
 	width: 400px;
@@ -16,8 +19,38 @@ const Title = styled.h1`
 `;
 
 function LoginForm() {
+	const { errors, isLoading, handleChange, handleSubmit } = useForm({
+		initialState: {
+			id: '',
+			password: '',
+		},
+		validate: ({ id, password }) => {
+			const newErrors: { [key: string]: string } = {};
+
+			if (!id) {
+				newErrors.id = '아이디를 입력해주세요.';
+			}
+
+			if (!password) {
+				newErrors.password = '비밀번호를 입력해주세요.';
+			}
+
+			return newErrors;
+		},
+		onSubmit: async () => {
+			try {
+				await sleep();
+				// eslint-disable-next-line no-console
+				console.log('submit success');
+			} catch (e) {
+				// eslint-disable-next-line no-console
+				console.log(e);
+			}
+		},
+	});
+
 	return (
-		<Container>
+		<Container onSubmit={handleSubmit}>
 			<Title>Login</Title>
 			<Input
 				type="text"
@@ -25,19 +58,24 @@ function LoginForm() {
 				height="20px"
 				name="id"
 				placeholder="Id"
+				onChange={handleChange}
 			/>
+			{errors.id && <ErrorText>{errors.id}</ErrorText>}
 			<Input
 				type="password"
 				width="100%"
 				height="20px"
 				name="password"
 				placeholder="Password"
+				onChange={handleChange}
 				style={{ marginTop: '8px' }}
 			/>
+			{errors.password && <ErrorText>{errors.password}</ErrorText>}
 			<Button
 				type="submit"
 				width="100%"
 				height="32px"
+				disabled={isLoading}
 				style={{ marginTop: '16px' }}
 			>
 				Login
