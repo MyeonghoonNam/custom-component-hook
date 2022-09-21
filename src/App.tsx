6b513c4c947ed6adac44518cfef44581e03f100d
MyeongHoonNam <codenmh0822@naver.com>
2022-09-21 15:37:07 +0900

    feat #3: LoginForm 컴포넌트 렌더링 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import Box from '@components/Box';
-import Input from '@components/Input';
-import Button from '@components/Button';
-import React from 'react';
+import LoginForm from '@components/LoginForm';
 
 function App() {
-	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		console.log('button click');
-	};
-
 	return (
 		<div>
-			<Box />
-			<br />
-			<Input type="text" width="150px" height="20px" />
-			<br />
-			<Button width="100px" height="40px" onClick={handleClick}>
-				Login
-			</Button>
+			<LoginForm />
 		</div>
 	);
 }
