6ff4e4c72af79d622e3a80eddefd5c16751fac0e
MyeongHoonNam <codenmh0822@naver.com>
2022-09-21 21:05:20 +0900

    feat #17: ContainerForm 컴포넌트 스토리북 구현

---
+++ src/stories/ContainerForm.stories.tsx
@@ -0,0 +1,10 @@
+import ContainerForm from '@components/ContainerForm';
+
+export default {
+	title: 'Components/ContainerForm',
+	component: ContainerForm,
+};
+
+export function Default() {
+	return <ContainerForm />;
+}
