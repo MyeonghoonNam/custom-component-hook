9144f23cdb85a707c8514322ba1fff78f39aa458
MyeongHoonNam <codenmh0822@naver.com>
2022-09-23 19:06:47 +0900

    feat #33: Spinner 컴포넌트 스토리북 구현

---
+++ src/stories/Spinner.stories.tsx
@@ -0,0 +1,15 @@
+import Spinner, { IProps } from '@components/Spinner';
+
+export default {
+	title: 'Components/Spinner',
+	component: Spinner,
+	argTypes: {
+		size: { defaultValue: 24, control: { type: 'number' } },
+		color: { control: { type: 'color' } },
+		bacgroundColor: { control: { type: 'color' } },
+	},
+};
+
+export function Default(args: IProps) {
+	return <Spinner {...args} />;
+}
