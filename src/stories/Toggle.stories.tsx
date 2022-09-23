e8d9538db94950b9cc6f0377d9e2cc55444c2913
MyeongHoonNam <codenmh0822@naver.com>
2022-09-23 21:08:23 +0900

    feat #35: Toggle 컴포넌트 스토리북 구현

---
+++ src/stories/Toggle.stories.tsx
@@ -0,0 +1,10 @@
+import Toggle, { IProps } from '@components/Toggle';
+
+export default {
+	title: 'Components/Toggle',
+	component: Toggle,
+};
+
+export function Default(args: IProps) {
+	return <Toggle {...args} />;
+}
