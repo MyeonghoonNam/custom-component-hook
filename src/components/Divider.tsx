1fcfe3bc1662fbb47d2d8143547a9e9eeb56ad43
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-10 21:22:29 +0900

    [Feature/#50] Divider 컴포넌트 구현 (#58)
    * feat #50: Divider 컴포넌트 구현
    
    * feat #50: Divider  컴포넌트 스토리북 구현

---
+++ src/components/Divider.tsx
@@ -0,0 +1,43 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export interface Props {
+	type: 'horizontal' | 'vertical';
+	size: number;
+}
+
+type ContainerProps = Pick<Props, 'size'>;
+
+const Container = styled.hr<ContainerProps>`
+	border: none;
+	background-color: #aaa;
+
+  &.vertical {
+    position relative;
+    top: -1px;
+    display: inline-block;
+    width: 1px;
+    height: 13px;
+    vertical-align: middle;
+
+    ${({ size }) => css`
+			margin: 0 ${size}px;
+		`}
+  }
+
+  &.horizontal {
+    display: block;
+    width: 100%;
+    height: 1px;
+
+    ${({ size }) => css`
+			margin: ${size}px 0;
+		`}
+  }
+`;
+
+function Divider({ type, ...props }: Props) {
+	return <Container className={type} {...props} />;
+}
+
+export default Divider;
---
+++ src/stories/Divider.stories.tsx
@@ -0,0 +1,27 @@
+import Divider from '@components/Divider';
+import Text from '@components/Text';
+
+export default {
+	title: 'Components/Divider',
+	component: Divider,
+};
+
+export function Horizontal() {
+	return (
+		<div>
+			<Text>Text 1</Text>
+			<Divider type="horizontal" size={8} />
+			<Text>Text 2</Text>
+		</div>
+	);
+}
+
+export function Vertical() {
+	return (
+		<div>
+			<Text>Text 1</Text>
+			<Divider type="vertical" size={8} />
+			<Text>Text 2</Text>
+		</div>
+	);
+}
