6c082eeef0accbab045e55121c58aa90b1bee040
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-12 23:00:44 +0900

    [Feature/#54] Tab 컴포넌트 구현 (#62)
    * chore #54: eslint rule update
    
    * feat #54: Tap 컴포넌트 구현
    
    * feat #54: TabItem 컴포넌트 구현
    
    * feat #54: Tap 컴포넌트 스토리북 구현

---
+++ src/components/Tap.tsx
@@ -0,0 +1,71 @@
+import styled from '@emotion/styled';
+import React, { useState, useMemo, useCallback } from 'react';
+import TabItem from './TabItem';
+
+export interface Props {
+	children: React.ReactNode;
+	active?: number;
+}
+
+const TabContainer = styled.div`
+	width: 420px;
+`;
+
+const TabItemContainer = styled.div`
+	border-bottom: 2px solid #dd;
+	background-color: #eee;
+`;
+
+function Tab({ children, active }: Props) {
+	const childrenToArray = useCallback((children: React.ReactNode) => {
+		return React.Children.toArray(children).filter((element) => {
+			if (React.isValidElement(element)) {
+				return true;
+			}
+
+			return false;
+		});
+	}, []);
+
+	const [currentActive, setCurrentActive] = useState(() => {
+		if (active) return active;
+
+		const item = childrenToArray(children)[0] as React.ReactElement;
+		return item.props.index;
+	});
+
+	const items = useMemo(() => {
+		return React.Children.toArray(children).map((element) => {
+			let item;
+
+			if (React.isValidElement(element)) {
+				item = React.cloneElement(element, {
+					...element.props,
+					key: element.props.index,
+					active: element.props.index === currentActive,
+					onClick: () => {
+						setCurrentActive(element.props.index);
+					},
+				});
+			}
+
+			return item;
+		});
+	}, [children, currentActive]);
+
+	const activeItem = useMemo(
+		() => items.find((element) => currentActive === element?.props.index),
+		[currentActive, items],
+	);
+
+	return (
+		<TabContainer>
+			<TabItemContainer>{items}</TabItemContainer>
+			<div>{activeItem?.props.children}</div>
+		</TabContainer>
+	);
+}
+
+Tab.Item = TabItem;
+
+export default Tab;
