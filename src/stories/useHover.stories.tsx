024322116db5dfd995986488eaf8bace0768f4d6
MyeongHoonNam <codenmh0822@naver.com>
2022-10-13 22:06:16 +0900

    feat #55: useHover custom hook 스토리북 구현

---
+++ src/stories/useHover.stories.tsx
@@ -0,0 +1,23 @@
+import useHover from '@hooks/useHover';
+import styled from '@emotion/styled';
+
+export default {
+	title: 'Hooks/useHover',
+};
+
+const Box = styled.div`
+	width: 100px;
+	height: 100px;
+	background-color: black;
+`;
+
+export function Default() {
+	const [boxRef, isHover] = useHover<HTMLDivElement>();
+
+	return (
+		<div>
+			<Box ref={boxRef} />
+			{isHover && <div>Tooltip</div>}
+		</div>
+	);
+}
