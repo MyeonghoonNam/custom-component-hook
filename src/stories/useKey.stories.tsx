aba26adc3cdc587c9f365680da3e4ef1abcafa14
MyeongHoonNam <codenmh0822@naver.com>
2022-10-14 15:53:35 +0900

    feat #65: useKey custom hook 스토리북 구현

---
+++ src/stories/useKey.stories.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-alert */
+import useKey from '@hooks/useKey';
+
+export default {
+	title: 'Hooks/useKey',
+};
+
+export const Default = () => {
+	useKey('keydown', 'f', () => {
+		alert('f key down !');
+	});
+
+	useKey('keyup', 'q', () => {
+		alert('q key up');
+	});
+};
