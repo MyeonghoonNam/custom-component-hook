74d0643ace7ab1414c54637bdb4683669370d32f
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-14 15:57:01 +0900

    [Featrue/#65] useKey, useKeyPress custom hook 구현 (#66)
    * feat #65: useKey custom hook 구현
    
    * feat #65: useKey custom hook 스토리북 구현
    
    * feat #65: useKeyPress custom hook 구현
    
    * feat #65: useKeyPress custom hook 스토리북 구현
    
    * chore #65: eslint rule update

---
+++ src/hooks/useKey.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useEffect } from 'react';
+
+type KeyboardEvents = 'keyup' | 'keydown';
+
+const useKey = (
+	event: KeyboardEvents,
+	targetKey: string,
+	handler: () => void,
+) => {
+	const handleKey = useCallback(
+		({ key }: KeyboardEvent) => {
+			if (key === targetKey) {
+				handler();
+			}
+		},
+		[targetKey, handler],
+	);
+
+	useEffect(() => {
+		window.addEventListener(event, handleKey);
+
+		return () => {
+			window.removeEventListener(event, handleKey);
+		};
+	}, [event, targetKey, handleKey]);
+};
+
+export default useKey;
