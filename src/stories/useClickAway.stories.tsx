5b8b111e5c0475e6a681abfcd270ce99561e8170
MyeongHoonNam <codenmh0822@naver.com>
2022-10-15 22:39:18 +0900

    feat #67: useClickAway custom hook 스토리북 구현

---
+++ src/stories/useClickAway.stories.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import useClickAway from '@hooks/useClickAway';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+
+export default {
+	title: 'Hooks/useClickAway',
+};
+
+type PopOverProps = { show: boolean };
+
+const PopOver = styled.div<PopOverProps>`
+	width: 200px;
+	height: 200px;
+	border: 2px solid black;
+	background-color: #eee;
+	display: ${({ show }) => (show ? 'block' : 'none')};
+`;
+
+export function Default() {
+	const [show, setShow] = useState(false);
+	const ref = useClickAway<HTMLDivElement>((e: Event) => {
+		const { tagName } = e.target as HTMLElement;
+
+		if (tagName !== 'BUTTON') {
+			setShow(() => false);
+		}
+	});
+
+	const handleClick = () => {
+		setShow((prev) => !prev);
+	};
+
+	return (
+		<div>
+			<button type="button" onClick={handleClick}>
+				{show ? 'Hide' : 'Show'}
+			</button>
+			<PopOver ref={ref} show={show}>
+				PopOver
+			</PopOver>
+		</div>
+	);
+}
