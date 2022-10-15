01bbede214259ca95e36098217efa106755a0eda
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-15 22:40:46 +0900

    [Feature/#67] useClickAway custom hook 구현 (#68)
    * feat #67: useClickAway custom hook 구현
    
    * feat #67: useClickAway custom hook 스토리북 구현
    
    * chore #67: eslint rule update

---
+++ src/hooks/useClickAway.tsx
@@ -0,0 +1,33 @@
+import { useRef, useEffect } from 'react';
+
+const events = ['mousedown', 'touchstart'];
+
+const useClickAway = <T extends HTMLElement>(handler: (e: Event) => void) => {
+	const ref = useRef<T>(null);
+
+	useEffect(() => {
+		const handleEvent = (e: Event) => {
+			const element = ref.current;
+
+			if (!element || element.contains(e.target as Node)) {
+				return;
+			}
+
+			handler(e);
+		};
+
+		for (const event of events) {
+			document.addEventListener(event, handleEvent);
+		}
+
+		return () => {
+			for (const event of events) {
+				document.removeEventListener(event, handleEvent);
+			}
+		};
+	}, [ref, handler]);
+
+	return ref;
+};
+
+export default useClickAway;
