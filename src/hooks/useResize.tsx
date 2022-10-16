032c6927879388e524ec04ae2ceec845b80c57c9
MyeongHoonNam <codenmh0822@naver.com>
2022-10-16 19:39:31 +0900

    feat #69: useResize custom hook 구현

---
+++ src/hooks/useResize.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+const useResize = (handler: (rect: DOMRectReadOnly) => void) => {
+	const ref = useRef(null);
+
+	useEffect(() => {
+		const element = ref.current;
+		if (!element) return;
+
+		const observer = new ResizeObserver((entries) => {
+			handler(entries[0].contentRect);
+		});
+
+		observer.observe(element);
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [ref, handler]);
+
+	return ref;
+};
+
+export default useResize;
