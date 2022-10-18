bd05813842778655542432c0479b3eb554cb6506
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-18 23:46:51 +0900

    [Feature/#75] useInterval custom hook 구현 (#76)
    * feat #75: useInterval custom hook 구현
    
    * feat #75: useInterval custom hook 스토리북 구현

---
+++ src/hooks/useInterval.tsx
@@ -0,0 +1,20 @@
+import { useRef, useEffect } from 'react';
+
+const useInterval = (callback: () => void, delay: number | null) => {
+	const savedCallback = useRef(callback);
+
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		if (!delay) return;
+
+		const tick = () => savedCallback.current();
+		const id = setInterval(tick, delay);
+
+		return () => clearInterval(id);
+	}, [delay]);
+};
+
+export default useInterval;
