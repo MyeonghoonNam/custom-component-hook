a37246079c94f9db8c3bbdc1f1a52aff45209e36
MyeongHoonNam <codenmh0822@naver.com>
2022-10-18 23:45:49 +0900

    feat #75: useInterval custom hook 스토리북 구현

---
+++ src/stories/useInterval.stories.tsx
@@ -0,0 +1,32 @@
+import useInterval from '@hooks/useInterval';
+import { useState } from 'react';
+
+export default {
+	title: 'Hooks/useInterval',
+};
+
+export function Default() {
+	const [status, setStatus] = useState(false);
+	const [time, setTime] = useState(0);
+
+	useInterval(
+		() => {
+			setTime((prev) => prev + 1);
+		},
+		status ? 1000 : null,
+	);
+
+	const handleClick = () => {
+		setStatus((state) => !state);
+		setTime(0);
+	};
+
+	return (
+		<div>
+			<button type="button" onClick={handleClick}>
+				{status ? 'Stop' : 'Start'}
+			</button>
+			Timer: {time}s
+		</div>
+	);
+}
