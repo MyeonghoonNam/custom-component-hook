64ac8366c563c8c4401eb112a45ee381d06ce648
MyeongHoonNam <codenmh0822@naver.com>
2022-10-19 22:41:41 +0900

    feat #79: Modal 컴포넌트 스토리북 구현

---
+++ src/stories/Modal.stories.tsx
@@ -0,0 +1,33 @@
+import Modal from '@components/Modal';
+import { useCallback, useState } from 'react';
+
+export default {
+	title: 'Components/Modal',
+	component: Modal,
+};
+
+export function Default() {
+	const [visible, setVisible] = useState(false);
+
+	const handleClick = useCallback(() => {
+		setVisible(() => true);
+	}, []);
+
+	const modalClose = useCallback(() => {
+		setVisible(() => false);
+	}, []);
+
+	return (
+		<div>
+			<button type="button" onClick={handleClick}>
+				Show Modal
+			</button>
+			<Modal visible={visible} onClose={modalClose}>
+				Modal
+				<button type="button" onClick={modalClose}>
+					close
+				</button>
+			</Modal>
+		</div>
+	);
+}
