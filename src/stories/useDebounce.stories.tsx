fb0aaab44342e36ee2a81747e5f05c8d59baef04
MyeongHoonNam <codenmh0822@naver.com>
2022-10-19 15:11:02 +0900

    feat #77: useDebounce custom hook 스토리북 구현

---
+++ src/stories/useDebounce.stories.tsx
@@ -0,0 +1,50 @@
+import useDebounce from '@hooks/useDebounce';
+import Input from '@components/Input';
+import React, { useCallback, useEffect, useState } from 'react';
+
+export default {
+	title: 'Hooks/useDebounce',
+};
+
+const suggest = ['Kakao', 'Naver', 'Coupang', 'Line'];
+
+export function Default() {
+	const [result, setResult] = useState<string[]>([]);
+	const [value, setValue] = useState('');
+	const debouncedValue = useDebounce<string>(value, 500);
+
+	useEffect(() => {
+		if (debouncedValue === '') {
+			setResult(() => []);
+		} else {
+			setResult(() =>
+				suggest.filter((company) =>
+					company.toLocaleLowerCase().includes(debouncedValue.toLowerCase()),
+				),
+			);
+		}
+	}, [debouncedValue]);
+
+	const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(() => e.target.value);
+	}, []);
+
+	return (
+		<div>
+			<Input
+				width="300px"
+				height="20px"
+				value={value}
+				onChange={handleChange}
+				autoFocus
+			/>
+
+			<div>
+				{result.map((item, index) => (
+					// eslint-disable-next-line react/no-array-index-key
+					<span key={index}>{item}</span>
+				))}
+			</div>
+		</div>
+	);
+}
