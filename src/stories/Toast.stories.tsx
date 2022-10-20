ce59d6424d2821b015113fc611f8aeca1b029e23
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-20 17:43:51 +0900

    [Feature/#81] Toast 컴포넌트 구현 (#82)
    * chore #81: uuid library install
    
    * chore #81: eslint rule update
    
    * feat #81: Toast 컴포넌트 구현
    
    * feat #81: ToastItem 컴포넌트 구현
    
    * feat #81: ToastManager 컴포넌트 구현
    
    * feat #81: Toast 컴포넌트 스토리북 구현

---
+++ src/stories/Toast.stories.tsx
@@ -0,0 +1,13 @@
+import Toast from '@components/Toast';
+
+export default {
+	title: 'Components/Toast',
+};
+
+export function Default() {
+	return (
+		<button type="button" onClick={() => Toast.show('안내드립니다', 3000)}>
+			Show Toast !
+		</button>
+	);
+}
